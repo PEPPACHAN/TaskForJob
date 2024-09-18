71ea79af82f4283aa661a78ce02fb1302b13b1d5
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-09-18 16:49:17 +0300

    Initial Commit

---
+++ TaskForJob/TaskForJobApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskForJobApp.swift
+//  TaskForJob
+//
+//  Created by PEPPA CHAN on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskForJobApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
